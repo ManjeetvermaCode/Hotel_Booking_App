c29ae18efaf8fe3d6e9814d7e6d82009dc711f17
manjeetverma <manjeetverma@gmail.com>
2023-11-09 18:49:09 +0530

    setted up redux and redux store

--- client/src/bookingpages/HomePage.js
+++ client/src/bookingpages/HomePage.js
@@ -1,7 +1,11 @@
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector"
+
+
 const Home=()=>{
+    const {rootReducer}=useSelector((state)=>state)
     return(
         <div className='container-fluid h1 text-center p-3'>
-            HomePage
+            HomePage {JSON.stringify(rootReducer.user)}
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 
+// 1. import from react-redux and redux
+
+import {configureStore} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+import { rootReducer } from './reducers/combineReducer';
+
+// 2. create user reducer function 
+
+
+// 3. combine multiple reducers
+
+// 4. create redux store
+const store=configureStore({
+  reducer:{
+    rootReducer,
+    composeWithDevTools
+  }
+  // composeWithDevTools()
+})
+
+// 5. provide redux store to entire app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>
 );
 
